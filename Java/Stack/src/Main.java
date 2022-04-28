f24e8bcb4df836206522d13866ab5bf8a36c2024
SeungGu Kang <alex9366@naver.com>
2022-04-29 07:41:31 +0900

    폴더 정리

--- Java/Stack/src/Main.java
+++ Java/Stack/src/Main.java
@@ -2,6 +2,7 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
+        //stack 클래스
         //stack 선언
         Stack<Integer> stack = new Stack<>(); //int형 스택 선언
         stack.push(1); //stack에 값 추가
@@ -12,5 +13,8 @@ public class Main {
         System.out.println(stack.peek());//stack의 가장 상단의 값 출력
         System.out.println(stack.empty()); //stack이 비어있는지 체크(비어있다면 true, 비어있지 않으면 false)
         System.out.println(stack.contains(1)); //stack에 1이 있는지 체크(있다면 true, 없다면 false)
+
+        //배열로 stack 구현
+
     }
 }
